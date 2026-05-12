fd8c25f4bcfd5732168d70531759a71a8e699dcd
Francesco Cassese <francescocassese9@gmail.com>
2026-05-12 16:25:10 +0200

    feat: Aggiunta navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,26 @@ import NotFound from "./pages/NotFound";
 import Homepage from "./pages/Homepage";
 import ChiSiamo from "./pages/ChiSiamo";
 import Prodotti from "./pages/Prodotti";
+import LayoutPage from "./layouts/LayoutPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* http://localhost:5173/ */}
-        <Route path="" element={<Homepage />} />
+        <Route element={<LayoutPage />}>
+          {/* http://localhost:5173/ */}
+          <Route path="" element={<Homepage />} />
 
-        {/* http://localhost:5173/chi-siamo */}
-        <Route path="chi-siamo" element={<ChiSiamo />} />
+          {/* http://localhost:5173/chi-siamo */}
+          <Route path="chi-siamo" element={<ChiSiamo />} />
 
-        {/* http://localhost:5173/prodotti */}
-        <Route path="prodotti" element={<Prodotti />} />
-
+          {/* http://localhost:5173/prodotti */}
+          <Route path="prodotti" element={<Prodotti />} />
+        </Route>
         {/* Pagina non trovata*/}
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 }
 export default App;
