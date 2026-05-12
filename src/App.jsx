4d1d7c827acea73f3c14bae12a282d834c6df3e2
Francesco Cassese <francescocassese9@gmail.com>
2026-05-12 15:48:51 +0200

    feat: aggiunti i precorsi alle pagine

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import NotFound from "./pages/NotFound";
+import Homepage from "./pages/Homepage";
+import ChiSiamo from "./pages/ChiSiamo";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* http://localhost:5173/ */}
+        <Route path="" element={<Homepage />} />
+
+        {/* http://localhost:5173/chi-siamo */}
+        <Route path="chi-siamo" element={<ChiSiamo />} />
+
+        {/* http://localhost:5173/prodotti */}
+        <Route path="prodotti" element={<ChiSiamo />} />
+
+        {/* Pagina non trovata*/}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
