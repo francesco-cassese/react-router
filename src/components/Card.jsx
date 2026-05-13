872c1b16dcc1d8a22a7433af7f82639dae2ce6a0
Francesco Cassese <francescocassese9@gmail.com>
2026-05-13 18:51:07 +0200

    refactor: ridimensionata pagina dettagli

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,16 +9,20 @@ function Card({ image, title, category, description, price, rating }) {
                 </div>
                 <div className="section-right mx-5">
                     <h1 className="display-6 fw-semibold mb-3">{title}</h1>
-                    <span className="badge bg-success mb-3">{category}</span>
-                    <h2 className="fw-bold">&euro;{price}</h2>
-                    <p>⭐ {rating?.rate} ({rating?.count})</p>
-                    <div className="product-description">
+                    <span className="badge bg-success-subtle text-success px-3 py-2 mb-3 rounded-pill">{category}</span>
+                    <h2 className="fw-bold text-success mb-3 display-6">&euro;{price}</h2>
+                    <div className="d-inline-flex align-items-center gap-2 mb-3">
+                        <span>⭐</span>
+                        <span className="fw-medium">{rating?.rate}</span>
+                        <span className="text-muted">({rating?.count})</span>
+                    </div>
+                    <div className="mt-4 p-3 bg-light rounded">
                         <h3>Descrizione</h3>
-                        <p>{description}</p>
+                        <p className={"text-secondary lh-lg mb-0"} >{description}</p>
                     </div>
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 export default Card
