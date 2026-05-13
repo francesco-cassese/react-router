65ab89ec9378b6053ed89ad7ccc733ab76bdc91f
Francesco Cassese <francescocassese9@gmail.com>
2026-05-13 02:56:20 +0200

    feat: ridimensionate le card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ function Card({ image, title, category, description, price, rating }) {
         < article className="card p-3 h-100 d-flex flex-column justify-content-between shadow" >
             <h5 className="card-title line-clamp">{title}</h5>
             <div className="container-img">
-                <img src={image} alt={title} className="d-block w-100" />
+                <img src={image} alt={title} className="d-block w-100 mb-2" />
             </div>
             <div className="container-info small">
                 <span className="badge rounded-pill bg-success mb-2 p-2">{category}</span>
